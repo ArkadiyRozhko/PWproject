7eb4668f0ae21b4561b36626b7a0f6aa73b1efc3
Arkadiy <arkasha9@gmail.com>
2016-10-11 11:59:26 +0300

    Add project files.

---
+++ PWproject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("**************************");
+            Console.WriteLine("******Мой проект*********");
+            Console.WriteLine("**************************");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine();
+            Console.WriteLine("Hello world!!!!!!!!!!!!!!");
+            Console.ReadLine();
+        }
+    }
+}

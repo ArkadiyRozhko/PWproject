b1b14c7c94ba63d5db7f2824227e833d0f1f2272
Arkadiy <arkasha9@gmail.com>
2016-10-11 13:38:12 +0300

    Add var Project

---
+++ VarType/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Vasya";
+            var age = 20;
+            var IsProgramer = true;
+
+
+            Type nameType = name.GetType();
+            Type ageType = age.GetType();
+            Type IsProgramerType = IsProgramer.GetType();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Тип name {0}",nameType);
+            Console.WriteLine("Тип age {0}", ageType);
+            Console.WriteLine("Тип IsProgramer {0}", IsProgramerType);
+
+            Console.ReadLine();
+
+        }
+    }
+}

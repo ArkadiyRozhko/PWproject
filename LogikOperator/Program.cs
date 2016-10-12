7ce5660112c3266f105f2f348edbaaf59f01a23b
Arkadiy <arkasha9@gmail.com>
2016-10-12 17:46:04 +0300

    Add proj logic

---
+++ LogikOperator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogikOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short d = 10, f = 12;
+            bool var1 = true, var2 = false;
+
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            if (d<f)
+            {
+                Console.WriteLine("d < f");
+            }
+
+            if (d<=f)
+            {
+                Console.WriteLine("d <= f");
+            }
+
+            if (d!=f)
+            {
+                Console.WriteLine("d != f");
+            }
+
+            if (d>f)
+            {
+                Console.WriteLine("d > f");
+            }
+
+            if (var1&var2)
+            {
+                Console.WriteLine("var1 & var2");
+            }
+
+            if (!(var1 & var2))
+            {
+                Console.WriteLine("! (var1 & var2) = true");
+            }
+
+            if (var1 | var2)
+            {
+                Console.WriteLine("var1 | var2 = true");
+            }
+
+            if (var1 ^ var2)
+            {
+                Console.WriteLine("var1 ^ var2 = true");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}

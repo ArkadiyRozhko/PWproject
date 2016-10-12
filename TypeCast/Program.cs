f52cba794549c72d28c3ce501021bc31de77801d
Arkadiy <arkasha9@gmail.com>
2016-10-12 11:15:00 +0300

    Cast

---
+++ TypeCast/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 410, i2 = 85420;
+            decimal dec = 12.123654m;
+
+            Console.WriteLine((short)i1);
+            Console.WriteLine((short)i2);
+
+            Console.WriteLine((int)dec);
+
+            Console.ReadLine();
+        }
+    }
+}

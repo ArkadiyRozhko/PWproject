b8aa33a68a991f0f77ecc0a3155faddb9e1a20dc
Arkadiy <arkasha9@gmail.com>
2016-10-12 14:18:49 +0300

    add project

---
+++ Ostatok/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ostatok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            float f1, f2;
+
+            num1 = 13 / 3;
+            num2 = 13 % 3;
+
+            f1 = 13.0f / 3.0f;
+            f2 = 13.0f % 3.0f;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Результат і залишок від ділення 13 на 3 = {0} - {1}",num1,num2);
+            Console.WriteLine("Результат ділення 13,0 на 3,0 = {0:#.###} - {1}",f1,f2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ inkrement_dekrement/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inkrement_dekrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short n = 1;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i+ n++ + "\t");
+                
+            }
+            Console.WriteLine();
+
+            n = 1;
+
+            for (int i = 0; i <10;  i++)
+            {
+                Console.Write(i+ ++n + "\t");
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}

9bde089340ea23e6064817fd2b16352e6be32f85
Arkadiy <arkasha9@gmail.com>
2016-10-19 16:10:29 +0300

    sw

--- switch/Program.cs
+++ switch/Program.cs
@@ -10,7 +10,32 @@ namespace @switch
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("введіть мову програмування (C#, VB або C++)");
 
+            string str = Console.ReadLine();
+            sw(str);
+            Console.ReadKey();
+
+            
+        }
+        static void sw(string st)
+        {
+            switch (st)
+            {
+                case "C#":
+                    Console.WriteLine("Мова С#");
+                    break;
+                case "VB":
+                    Console.WriteLine("Мова VB");
+                    break;
+                case "C++":
+                    Console.WriteLine("Мова С++");
+                    break;
+                
+                default:
+                    Console.WriteLine("не правильне введення!");
+                    break;
+            }
         }
     }
 }

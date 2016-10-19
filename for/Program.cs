613f9a15009e9dc2b8a417ce5296ea11f7623468
Arkadiy <arkasha9@gmail.com>
2016-10-19 17:19:51 +0300

    add for1

---
+++ for/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть розмірність таблиці");
+
+            int num = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <=num; i++)
+            {
+                for (int j = 1; j <=num; j++)
+                {
+                    Console.Write("{0}\t",i*j);
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}

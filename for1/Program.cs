27fc7625b035240a9031d1f74bce6e0e9208395c
Arkadiy <arkasha9@gmail.com>
2016-10-19 18:17:37 +0300

    for2

---
+++ for1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0 , j=100; i <= j; i+=2,j-=2)
+            {
+                Console.WriteLine("i ={0} --- j ={1} ",i,j);
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}

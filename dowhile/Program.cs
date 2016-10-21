5b33c50c4082b36918cf87bb05307febb64257e3
Arkadiy <arkasha9@gmail.com>
2016-10-21 16:37:18 +0300

    do while factorial

---
+++ dowhile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Факторіал числа!!!");
+                int i, num = 1, result = 1;
+
+                Console.WriteLine("Введіть число");
+                i = Int32.Parse(Console.ReadLine());
+
+                do
+                {
+                    result *= num;
+                    num++;
+                    Console.WriteLine(result);
+                } while (num <= i);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Ви ввели не число {0}", ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+            
+
+
+        }
+    }
+}

17ec28bc75e91ae379adca1d458def0c6902ce45
Arkadiy <arkasha9@gmail.com>
2016-10-21 09:57:31 +0300

    Add while project

---
+++ while/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte k = 2, i = 0;
+            int result = 1;
+
+            while (i<30)
+            {
+                result *= k;
+                i++;
+                Console.WriteLine("{0} В степені {1}={2}",i,k,result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

3f1b6759d2a365baf1b77380b2559ecf10323dcd
Arkadiy <arkasha9@gmail.com>
2016-10-24 13:23:38 +0300

    Add masiv obj

---
+++ MassivObjekt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassivObjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] arr = { true, "one", 12};
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine("{0} тип элемента - {1}",item,item.GetType());
+            }
+            Console.ReadKey();
+        }
+    }
+}

77f20be4cc73e8403d2957222addc2479cebacd4
ArkadiyRozhko <arkasha9@gmail.com>
2016-12-21 10:18:35 +0200

    Add compare string

--- CompareString/Program.cs
+++ CompareString/Program.cs
@@ -66,6 +66,31 @@ namespace CompareString
                 Console.WriteLine("Строки равны");
             }
 
+            Console.WriteLine();
+
+            if (str1==str4)
+            {
+                Console.WriteLine(str1 + "=" + str4);
+            }
+            if (str1!=str3)
+            {
+                Console.WriteLine(str1 + "!=" + str3);
+            }
+            if (str1!=str2)
+            {
+                Console.WriteLine(str1 + "!=" + str2);
+            }
+
+            if (String.Equals(str1,str2,StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.WriteLine("Сравнение строк методом Equals() с параметром CurrentCultureIgnoreCase \n" + str1 +" равно " +str2 );
+                Console.WriteLine();
+            }
+
+            if ((String.Compare(str2,0,str5,0,3))>0)
+            {
+                Console.WriteLine("Первые 3 символа строки " + str2 + " больше первых 3-х символов строки" + str5);
+            }
         }
     }
 }

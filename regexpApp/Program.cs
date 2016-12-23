00ce08b83060ffca1fda39e5148a256776e65bdb
ArkadiyRozhko <arkasha9@gmail.com>
2016-12-23 13:44:13 +0200

    Add regex serch

---
+++ regexpApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace regexpApp
+{
+    class Program
+    {
+        public static object Regexp { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string[] strs = new string[] { "Wuck World", "Hello world", "My wonderful world" };
+
+            Regex regexp = new Regex("World");
+
+            Console.WriteLine("Регистрозависимый поиск:");
+
+            foreach (var item in strs)
+            {
+                if (regexp.IsMatch(item))
+                {
+                    Console.WriteLine("Есть такое слово в строке {0}",item);
+                }
+            }
+
+            Console.WriteLine();
+
+            regexp = new Regex("World", RegexOptions.IgnoreCase);
+
+            Console.WriteLine("Регистронезависимый поиск:");
+
+            foreach (var item in strs)
+            {
+                if (regexp.IsMatch(item))
+                {
+                    Console.WriteLine("Есть такое слово в строке {0}",item);
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

c50d4ceb2280cbec18775bea87a1f11991d49cf1
ArkadiyRozhko <arkasha9@gmail.com>
2016-12-23 16:13:58 +0200

    Match Add

---
+++ RegexMath/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "1 кг яблок стоит 20 грн" + "1 кг мандарин стоит 40 грн" + "1 кг апельсин стоит 45 грн" + "1 кг мяса стоит 80 грн";
+
+            string patern = @"\b(\d+\W?грн)";
+
+            Regex regex = new Regex(patern);
+
+            Match match = regex.Match(input);
+
+            while (match.Success)
+            {
+                Console.WriteLine(match.Groups[1].Value);
+
+               match= match.NextMatch();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

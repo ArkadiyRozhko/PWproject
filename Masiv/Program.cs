8961c2536f2c3c4db0a30f0f969651be67dfb9bc
ArkadiyRozhko <arkasha9@gmail.com>
2016-12-24 13:02:50 +0200

    add masiv

---
+++ Masiv/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass=new int[5];
+            mass[0] = 125;
+            mass[1] = 3;
+            mass[2] = 456;
+            mass[3] = 32;
+            mass[4] = 21;
+
+            foreach (var item  in mass)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}

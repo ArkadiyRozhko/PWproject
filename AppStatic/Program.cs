9e26769c35caea9e54b0cfda970f3d233d11c47a
ArkadiyRozhko <arkasha9@gmail.com>
2017-01-05 10:30:20 +0200

    Static ключевое слово

---
+++ AppStatic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStatic
+{
+    class Program
+    {
+        class MyCircle
+        {
+            public static double SqrCircle(double radius)
+            {
+                return Math.PI * radius * radius;
+            }
+            public static double LongCircle(double radius)
+            {
+                return 2 * Math.PI * radius;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+
+            double r = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Площадь круга = {0}",MyCircle.SqrCircle(r));
+            Console.WriteLine("Длинна окружности = {0}", MyCircle.LongCircle(r));
+            Console.ReadLine();
+
+        }
+    }
+}
